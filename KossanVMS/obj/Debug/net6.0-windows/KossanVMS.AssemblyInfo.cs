71d592684a688c33165e9b082b8d89ee664d84f2
Anderu123 <evilfire8laster@live.com>
2025-10-17 17:51:18 +0800

    Update

--- KossanVMS/obj/Debug/net6.0-windows/KossanVMS.AssemblyInfo.cs
+++ KossanVMS/obj/Debug/net6.0-windows/KossanVMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KossanVMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+197ad4455faf9f8d474c5ac0c3e624f0719e2da3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c44e2cabc0998de3cb67a4dde274772843e4a918")]
 [assembly: System.Reflection.AssemblyProductAttribute("KossanVMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KossanVMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
